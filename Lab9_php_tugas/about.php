799548859444633663a0ec407eed1504cec521d3
Fahmi Prayoga <56239989+coraid12@users.noreply.github.com>
2021-06-06 14:53:20 +0700

    Add files via upload

---
+++ Lab9_php_tugas/about.php
@@ -0,0 +1,8 @@
+<?php require('header.php'); ?>
+
+<div class="content">
+    <h2>Fahmi Prayoga</h2>
+    <p>Ini adalah halaman about hasil praktikum 9 pemrograman web</p>
+</div>
+
+<?php require('footer.php'); ?>
---
+++ Lab9_php_tugas/footer.php
@@ -0,0 +1,6 @@
+<footer>
+            <p>&copy; 2021, 2021, Informatika, Universitas Pelita Bangsa, Fahmi Prayoga, TI.19.A.2</p>
+        </footer>
+    </div>
+</body>
+</html>
